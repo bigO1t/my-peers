e551a6229b895890e3558f36cfcdec4cb2931641
bhaveshmit <bhavesh_p@users.noreply.github.com>
2018-11-07 17:51:59 -0600

    Added content for 404 page

--- src/app/errors/404/app404error/app404error.component.ts
+++ src/app/errors/404/app404error/app404error.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-app404error',
   templateUrl: './app404error.component.html',
-  styleUrls: ['./app404error.component.css']
+  styleUrls: ['./app404error.component.scss']
 })
 export class App404errorComponent implements OnInit {
 
