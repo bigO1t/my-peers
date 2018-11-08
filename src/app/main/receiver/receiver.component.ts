287ff6e62100e1c9cbdec2abd427e62b8c11e8f7
Perera <Nelma_Perera@Dell.com>
2018-11-08 10:42:57 -0600

    receiver

--- src/app/main/receiver/receiver.component.ts
+++ src/app/main/receiver/receiver.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/@shared-module/interfaces/user.interface';
+import { Router } from '@angular/router';
+import { UserService } from 'src/@shared-module/services/user.service';
+import { DataService } from 'src/@shared-module/services/data.service';
 
 @Component({
   selector: 'app-receiver',
@@ -6,7 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receiver.component.scss']
 })
 export class ReceiverComponent implements OnInit {
-  constructor() {}
+  user = <IUser>{};
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private dataService: DataService
+  ) {
+    this.user = this.userService.currentUser;
+  }
 
   ngOnInit() {}
+
+  finish() {
+    this.dataService.search(this.user.paired_user, 'users').subscribe(doc => {
+      if (doc.exists) {
+        let pairedUser = doc.data();
+        this.dataService.deleteField('paired_user', pairedUser.key, 'users');
+        //pairedUser.paired_user = null;
+        this.dataService.addUpdateData('users', pairedUser);
+      }
+      this.dataService.deleteField('paired_user', this.user.key, 'users');
+      this.dataService.addUpdateData('users', this.user);
+    });
+  }
 }
